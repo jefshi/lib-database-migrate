c8171aa3feb6ec6d68b69f277a7399741368fb81
jefshi <jefshi@yeah.net>
2018-07-07 19:03:14 +0800

    初始化项目结构

---
+++ libforupdate/src/main/java/com/csp/db/update/lib/MigrateHelper.java
@@ -0,0 +1,11 @@
+package com.csp.db.update.lib;
+
+/**
+ * 数据库升级时数据迁移帮助类
+ * Created by csp on 2018/07/07.
+ * Modified by csp on 2018/07/07.
+ *
+ * @version 1.0.0
+ */
+public class MigrateHelper {
+}
